682ae8f1245c170f19374d5f9a13dd2004394adc
justintsugranes <justintsugranes@proton.me>
2022-10-18 09:23:21 -0400

    setup project according to course

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -3,11 +3,13 @@ require('@nomiclabs/hardhat-ethers')
 require('dotenv').config()
 require('@nomiclabs/hardhat-etherscan')
 require('./tasks/block-number')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || 'https://eth-goerli'
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
@@ -18,9 +20,22 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 5,
         },
+        localhost: {
+            url: 'http://127.0.0.1:8545/',
+            chainId: 31337,
+        },
     },
     solidity: '0.8.8',
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        // switch to true if you want to use gas reporter
+        enabled: false,
+        outputFile: 'gas-report.txt',
+        noColors: true,
+        currency: 'USD',
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: 'MATIC',
     },
 }
